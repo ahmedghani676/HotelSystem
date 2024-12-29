9f753b037ab191d782eb0118bda1de3d8c6e5f44
ahmedghani676 <58558139+ahmedghani676@users.noreply.github.com>
2024-12-30 01:03:24 +0500

    add

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    include: ['swiper'],  // Ensure swiper is included in dependencies optimization
-  },
-  build: {
-    rollupOptions: {
-      external: ['swiper/css', 'swiper/css/pagination', 'swiper/css/navigation'],  // Externalize swiper CSS
-    },
-  },
-});
+})
