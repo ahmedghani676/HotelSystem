440a210f3effb57a048a0d3c7c1e710d90955454
ahmedghani676 <58558139+ahmedghani676@users.noreply.github.com>
2025-02-28 20:21:21 +0500

    add

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,28 +1,36 @@
-import React from 'react'
-import aboutCSS from './../About/About.module.css'
-import aboutImg from './../../assets/about-one.png'
+import React from "react";
+import aboutCSS from "./../About/About.module.css";
+import aboutImg from "./../../assets/12.jpg";
 
 const About = () => {
   return (
-      <div id="about" className={`${aboutCSS.about_wrapper} section`}>
+    <div id="about" className={`${aboutCSS.about_wrapper} section`}>
       <div className={aboutCSS.about_img}>
         <img src={aboutImg} alt="" />
       </div>
       <div className={aboutCSS.about_content}>
-        <small className='section_heading'>Wisam International Hotel</small>
-        <h2 className='section_title'>
+        <small className="section_heading">Wisam International Hotel</small>
+        <h2 className="section_title">
           Where Elegance meets <span>Excellence</span>
         </h2>
         <p>
-          Welcome to Wisam International Hotel, where luxury and comfort converge. Nestled in the heart of the city, our hotel offers a unique blend of modern amenities and timeless elegance. Whether you're here for a business trip, family vacation, or special event, we promise an unforgettable stay with impeccable service and attention to detail.
+          Welcome to Wisam International Hotel, where luxury and comfort
+          converge. Nestled in the heart of the city, our hotel offers a unique
+          blend of modern amenities and timeless elegance. Whether you're here
+          for a business trip, family vacation, or special event, we promise an
+          unforgettable stay with impeccable service and attention to detail.
         </p>
         <div className={aboutCSS.Cards}>
-          <p>250k+ <span>Happy Visitors</span></p>
-          <p>100+ <span>Memorable Events</span></p>
+          <p>
+            250k+ <span>Happy Visitors</span>
+          </p>
+          <p>
+            100+ <span>Memorable Events</span>
+          </p>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default About
+export default About;
--- src/components/Amenities/Amenities.jsx
+++ src/components/Amenities/Amenities.jsx
@@ -1,20 +1,21 @@
-import React from 'react'
-import { Swiper, SwiperSlide } from 'swiper/react';
-import AminitiesCss from './../Amenities/Amenities.module.css'
-import img1 from './../../assets/gym.jpg'
-import img2 from './../../assets/pool.jpg'
-import img3 from './../../assets/rest.jpg'
-import { Autoplay } from 'swiper/modules'
-import { Link } from 'react-scroll'; // Import Link from react-scroll
-import "swiper/css";  
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import AminitiesCss from "./../Amenities/Amenities.module.css";
+import img1 from "./../../assets/gym.jpg";
+import img2 from "./../../assets/pool.jpg";
+import img3 from "./../../assets/rest.jpg";
+import { Autoplay } from "swiper/modules";
+import { Link } from "react-scroll"; // Import Link from react-scroll
+import "swiper/css";
 
 export const Amenities = () => {
   return (
-    <div id = "amenities" className={`${AminitiesCss.aminitiesCss_wrapper} section`}>
-      <small className='section_heading'>
-        Luxury Amenities
-      </small>
-      <h2 className='section_title'>
+    <div
+      id="amenities"
+      className={`${AminitiesCss.aminitiesCss_wrapper} section`}
+    >
+      <small className="section_heading">Luxury Amenities</small>
+      <h2 className="section_title">
         Our Best <span>Amenities</span>
       </h2>
       <Swiper
@@ -24,40 +25,48 @@ export const Amenities = () => {
         parallax={true}
         speed={2000}
         autoplay={{
-          delay: 1000
+          delay: 1000,
         }}
         modules={[Autoplay]}
-        className={`${AminitiesCss.swiper}`}>
+        className={`${AminitiesCss.swiper}`}
+      >
         <SwiperSlide>
           <div className={`${AminitiesCss.amenities_item}`}>
             <img src={img1} alt="amenities-img" />
             <div className={AminitiesCss.content}>
               <h2>Swimming Pool</h2>
               <p>
-                Dive into luxury with our stunning swimming pool, designed for relaxation and recreation. Whether you're swimming laps or just soaking up the sun, our pool area offers a perfect escape with breathtaking views and top-notch facilities.
-                          </p>
-             <Link to="rooms" smooth={true} duration={500}>
-              <button>Book Now</button>
-            </Link>      
+                Dive into luxury with our stunning swimming pool, designed for
+                relaxation and recreation. Whether you're swimming laps or just
+                soaking up the sun, our pool area offers a perfect escape with
+                breathtaking views and top-notch facilities.
+              </p>
+              <Link to="rooms" smooth={true} duration={500}>
+                <button>Book Now</button>
+              </Link>
             </div>
           </div>
         </SwiperSlide>
-      
+
         <SwiperSlide>
           <div className={`${AminitiesCss.amenities_item}`}>
             <img src={img3} alt="amenities-img" />
             <div className={AminitiesCss.content}>
               <h2>Restaurant and Cafe</h2>
               <p>
-                Indulge in a culinary journey at our on-site restaurant and cafe, offering a delightful selection of international and local dishes. Whether you're in the mood for a cozy coffee or an elegant dinner, our dining experience is sure to satisfy your cravings.
+                Indulge in a culinary journey at our on-site restaurant and
+                cafe, offering a delightful selection of international and local
+                dishes. Whether you're in the mood for a cozy coffee or an
+                elegant dinner, our dining experience is sure to satisfy your
+                cravings.
               </p>
               <Link to="rooms" smooth={true} duration={500}>
-              <button>Book Now</button>
-            </Link> 
+                <button>Book Now</button>
+              </Link>
             </div>
           </div>
         </SwiperSlide>
       </Swiper>
     </div>
-  )
-}
+  );
+};
